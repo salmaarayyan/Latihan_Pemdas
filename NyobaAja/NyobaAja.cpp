e247eb2d68c550f956527c42ed188c9c29a43174
salmaarayyan <salmaarayyan@gmail.com>
2024-02-26 17:42:07 +0700

    Add project files.

---
+++ NyobaAja/NyobaAja.cpp
@@ -0,0 +1,20 @@
+// NyobaAja.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{//begin
+    // numeric nSisi, nLuas
+    // display 'masukan Sisi ='
+    // accept nSisi
+    // compute nLuas = nSisi * nSisi
+    // display 'Luasnya adalah =' +nLuas 
+
+    int nSisi, nLuas;
+    cout << "masukan Sisi = ";
+    cin >> nSisi;
+    nLuas = nSisi * nSisi;
+    cout << "Luasnya adalah = " << nLuas << endl;
+}//end
