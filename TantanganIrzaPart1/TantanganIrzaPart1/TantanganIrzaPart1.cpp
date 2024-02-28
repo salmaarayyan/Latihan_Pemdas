681080a79ac86f38733c43d30f5f95fecbb44e75
salmaa rifhani rayyan <160484911+salmaarayyan@users.noreply.github.com>
2024-02-28 17:37:29 +0700

    Add files via upload

---
+++ TantanganIrzaPart1/TantanganIrzaPart1/TantanganIrzaPart1.cpp
@@ -0,0 +1,37 @@
+// sebagai cacatan gunakan (int) untuk avg dan gunakan (double) untuk f1 dan d1
+// jangan lupa diberi jarak 
+		// numeric avg
+		// numeric f1=8e4
+		// numeric d1=2e4
+		// display 'bagilah antara 2 variabel berikut'
+		// display 'nilai f1' + f1 
+		// display 'nilai d1' + d1 
+		// display 'maka f1/d1'
+		// compute avg = f1/d1
+		// display 'masukan nilai f1 = ' + f1
+		// display 'masukan nilai d1 = ' + d1
+		// display 'maka hasilnya adalah = ' + avg
+// selesai 
+// semangat sayangkuu
+
+#include <iostream>
+using namespace std;
+int main()
+{
+int avg;
+double f1 = 8e4;
+double d1 = 2e4;
+cout << "bagilah antara 2 variabel berikut\n";
+cout << "nilai f1 = " << f1 << endl;
+cout << "nilai d1 = " << d1 << endl;
+cout << "maka f1/d1" << endl;
+avg = f1/d1;
+cout << "masukan nilai f1 = " << f1 << endl;
+cout << "masukan nilai d1 = " << d1 << endl;
+cout << "maka hasilnya adalah = " << avg << endl;
+
+
+
+ 
+}
+
