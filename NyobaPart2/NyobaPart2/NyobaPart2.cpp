e0b9f96268fcf56ebbfa3e798cb2b83f9b0541b9
salmaa rifhani rayyan <160484911+salmaarayyan@users.noreply.github.com>
2024-02-28 17:38:55 +0700

    Add files via upload

---
+++ NyobaPart2/NyobaPart2/NyobaPart2.cpp
@@ -0,0 +1,24 @@
+// NyobaPart2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std; 
+int main()
+{// begin
+    // numeric x,y,sum
+    //display 'masukan nilai 1' 
+    //accept x
+    // display 'masukan nilai 2'
+    //accept y
+    // compute sum = x + y
+    //display 'sum is =' + sum
+
+    int x, y, sum;
+    cout << "masukan nilai 1 =";
+    cin >> x;
+    cout << "masukan nilai 2 =";
+    cin >> y;
+    sum = x + y;
+    cout << "sum is =" << sum << endl;
+}//end 
+
